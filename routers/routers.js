fc72a89504e4aa331f6f14d9b8b37aeaf7f4026f
GoMingv <70299101@qq.com>
2018-09-21 19:22:15 +0800

    新增注册登录

--- routers/routers.js
+++ routers/routers.js
@@ -8,17 +8,17 @@ const router = new Router
 //设计主页
 router.get("/",async (cxt)=>{
     //需要 title
-    console.log(cxt)
     await cxt.render('index',{
         title:"首页"
     })
+
 })
 
 
 //动态路由 用户登录 用户注册
 router.get(/^\/user\/(?=reg|login)/,async (cxt)=>{
     //show 为 true 则显示注册 false 显示登录
-
+    console.log(11)
     const show = /reg$/.test(cxt.path)
     await cxt.render('register',{show})
 
